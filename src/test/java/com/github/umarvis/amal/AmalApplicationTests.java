4f00eea395e67f34fcbdb679e015cbfccdf38b7e
umarvis <umar_9292@mail.ru>
2024-05-13 23:11:13 +0300

    first push

---
+++ src/test/java/com/github/umarvis/amal/AmalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.umarvis.amal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
